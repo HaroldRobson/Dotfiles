b9155d0fbe6dd85006687a2418f5da412976248f
Harold <hrldrobson@gmail.com>
2026-04-13 11:40:02 +0100

    removed annoying surround text stuff from nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldcolumn = "0"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldtext = ""
+vim.opt.foldnestmax = 4
